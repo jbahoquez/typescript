55eafcb300f492dd767956838579e5113aeea66a
jbahoquez <jbahoquez@gmail.com>
2023-06-02 09:01:18 -0500

    Initial commit

---
+++ index.ts
@@ -0,0 +1,10 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+let a: number | 'Hola';
+
+a = 'Hola';
